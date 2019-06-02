4e9abb6c29365da631a59aa0fb8354924e65bb2e
Eugene <akakeevich@gmail.com>
2019-06-03 01:06:43 +0700

    Main.c upgrade

--- main.c
+++ main.c
@@ -4,18 +4,13 @@
 
 int main()
 {
-	float x,y;
-	float r;
+	float x,y,r;
 	printf("Напишите координаты для окружности x and y, r :\n");
 	scanf("%f%f%f", &x, &y, &r);
 	error(x,y,r);	
 	printf("Координаты вашей окружности: (%.4f %.4f, %.4f)\n", x, y, r);	
-	float pi, P, S;
-	pi = 3.1415;
-	P = 2*pi*r;
-	S = pi* r*r;
-	printf("Площадь = %.4f\n", S);
-	printf("Периметр = %.4f\n", P);
+	printf("Площадь = %.4f\n", S(r));
+	printf("Периметр = %.4f\n", P(r));
 	return 0;
 }
 
